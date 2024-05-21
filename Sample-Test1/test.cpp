16c2ce9e3f3ae4def0cff7f9d148cc52a156fb9b
quokkaHunter <ekdlemdjq456@naver.com>
2024-05-21 09:40:22 +0900

    [Feature] develop getMinus in Calc

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,10 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "../Project3/calc.cpp"
 
-TEST(TestCaseName, TestName) {
-	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
+TEST(CalculatorTest, MinusTest) {
+	Calc calc;
+	EXPECT_EQ(0, calc.getMinus(1, 1));
+	EXPECT_EQ(1, calc.getMinus(2, 1));
+	EXPECT_EQ(-1, calc.getMinus(1, 2));
 }
