46689dcc55abd6f5b354c1dcc60a8609395ee821
sp <hsp8405@naver.com>
2024-05-21 09:37:07 +0900

    [Feature] implement getZegop()

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,17 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "../Project3/calc.cpp"
 
 TEST(TestCaseName, TestName) {
 	EXPECT_EQ(1, 1);
 	EXPECT_TRUE(true);
+}
+
+TEST(TestCal, getZegopTest) {
+	Calc calc;
+	EXPECT_EQ(1, calc.getZegop(1));
+	EXPECT_EQ(4, calc.getZegop(2));
+	EXPECT_EQ(9, calc.getZegop(3));
+	EXPECT_EQ(16, calc.getZegop(4));
+	EXPECT_EQ(25, calc.getZegop(5));
 }
