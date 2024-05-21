4d91dee1a89663e1641a84ec482020f02b41208a
tamongbo <tamongbo@gmail.com>
2024-05-21 09:26:32 +0900

    initial commit

---
+++ Project3/calc.cpp
@@ -0,0 +1,29 @@
+#include <stdexcept>
+using namespace std;
+
+class Calc {
+public:
+	int getSum(int a, int b) {
+		return 0;
+	}
+	
+	int getGop(int a, int b) {
+		return 0;
+	}
+	
+	int getZegop(int a) {
+		return 0;
+	}
+	
+	int getMinus(int a, int b) {
+		return 0;
+	}
+	
+	int getDivide(int a, int b) {
+		return 0;
+	}
+	
+	int getSumSum(int a, int b, int c) {
+		return 0;
+	}
+};
